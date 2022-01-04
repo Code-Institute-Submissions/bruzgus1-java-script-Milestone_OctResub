2d03aa79dfe8ffd0e4f7a233c79bd303fe05ce9f
bruzgus1 <poviliukas987412365@gmail.com>
2022-01-04 13:53:32 +0000

    added a way to write in the second number

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let num1 = document.getElementById("num1")
 let operator = document.getElementById("operator")
+let num2 = document.getElementById("num2")
 
 // replaces the answer box numbers and operators to normal state
 
@@ -9,87 +10,209 @@ function erase() {
     } else {
         num1.innerHTML = "0"
         operator.innerHTML = ""
+        num2.innerHTML = ""
     }
 }
 
-// adds the correct numbers and operators in the answer box
+// adds the correct numbers in the answer box
+
+function zero() {
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+
+        } else {
+            num1.innerHTML += "0"
+        }
+    } else {
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "0."
+        } else {
+            num2.innerHTML += "0"
+        }
+    }
+}
 
 function one() {
-    if (num1.innerHTML === "0") {
-        num1.innerHTML = "1"
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+            num1.innerHTML = "1"
+        } else {
+            num1.innerHTML += "1"
+        }
     } else {
-        num1.innerHTML += "1"
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "1"
+        } else {
+            num2.innerHTML += "1"
+        }
     }
 }
 
 function two() {
-    if (num1.innerHTML === "0") {
-        num1.innerHTML = "2"
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+            num1.innerHTML = "2"
+        } else {
+            num1.innerHTML += "2"
+        }
     } else {
-        num1.innerHTML += "2"
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "2"
+        } else {
+            num2.innerHTML += "2"
+        }
     }
 }
 
 function three() {
-    if (num1.innerHTML === "0") {
-        num1.innerHTML = "3"
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+            num1.innerHTML = "3"
+        } else {
+            num1.innerHTML += "3"
+        }
     } else {
-        num1.innerHTML += "3"
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "3"
+        } else {
+            num2.innerHTML += "3"
+        }
     }
 }
 
 function four() {
-    if (num1.innerHTML === "0") {
-        num1.innerHTML = "4"
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+            num1.innerHTML = "4"
+        } else {
+            num1.innerHTML += "4"
+        }
     } else {
-        num1.innerHTML += "4"
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "4"
+        } else {
+            num2.innerHTML += "4"
+        }
     }
 }
 
 function five() {
-    if (num1.innerHTML === "0") {
-        num1.innerHTML = "5"
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+            num1.innerHTML = "5"
+        } else {
+            num1.innerHTML += "5"
+        }
     } else {
-        num1.innerHTML += "5"
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "5"
+        } else {
+            num2.innerHTML += "5"
+        }
     }
 }
 
 function six() {
-    if (num1.innerHTML === "0") {
-        num1.innerHTML = "6"
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+            num1.innerHTML = "6"
+        } else {
+            num1.innerHTML += "6"
+        }
     } else {
-        num1.innerHTML += "6"
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "6"
+        } else {
+            num2.innerHTML += "6"
+        }
     }
 }
 
 function seven() {
-    if (num1.innerHTML === "0") {
-        num1.innerHTML = "7"
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+            num1.innerHTML = "7"
+        } else {
+            num1.innerHTML += "7"
+        }
     } else {
-        num1.innerHTML += "7"
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "7"
+        } else {
+            num2.innerHTML += "7"
+        }
     }
 }
 
 function eight() {
-    if (num1.innerHTML === "0") {
-        num1.innerHTML = "8"
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+            num1.innerHTML = "8"
+        } else {
+            num1.innerHTML += "8"
+        }
     } else {
-        num1.innerHTML += "8"
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "8"
+        } else {
+            num2.innerHTML += "8"
+        }
     }
 }
 
 function nine() {
-    if (num1.innerHTML === "0") {
-        num1.innerHTML = "9"
+    if (operator.innerHTML === "") {
+        if (num1.innerHTML === "0") {
+            num1.innerHTML = "9"
+        } else {
+            num1.innerHTML += "9"
+        }
     } else {
-        num1.innerHTML += "9"
+        if (num2.innerHTML === "") {
+            num2.innerHTML = "9"
+        } else {
+            num2.innerHTML += "9"
+        }
     }
 }
+// add correct operators in the answer box
 
 function plus() {
     if (num1.innerHTML === "0") {
 
     } else {
         operator.innerHTML = "+"
+    }
+}
+
+function minus() {
+    if (num1.innerHTML === "0") {
+
+    } else {
+        operator.innerHTML = "-"
+    }
+}
+
+function multiply() {
+    if (num1.innerHTML === "0") {
+
+    } else {
+        operator.innerHTML = "x"
+    }
+}
+
+function divide() {
+    if (num1.innerHTML === "0") {
+
+    } else {
+        operator.innerHTML = "÷"
+    }
+}
+
+function decimal() {
+    if (num1.innerHTML === "0") {
+        num1.innerHTML += "."
+    } else {
+
     }
 }
